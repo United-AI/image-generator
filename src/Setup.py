3432690f64dcd5b0a088fba544dae6110a740488
John Huy Tran <johntran69444@outlook.de>
2021-10-26 00:32:04 +0200

    Update for color support

--- src/Setup.py
+++ src/Setup.py
@@ -1,12 +1,11 @@
 class Setup:
     def __init__(self):
         self.jpg_path = "" #Datensatz path
-        self.jpg_file_start = 0
-        self.jpg_file_end = 50000
+        self.amount_files = 10000
         self.BUFFER_SIZE = 60000
-        self.BATCH_SIZE = 256
+        self.BATCH_SIZE = 128
+        self.IMAGE_SIZE = 120 # reduce this to increase performance
         self.checkpoint_dir = "" #output safepoint
-        self.EPOCHS = 300
+        self.EPOCHS = 10000
         self.num_examples_to_generate = 16
         self.output_dir = "" #output folder
-        self.gifmaker_input = "" #input gifmaker
