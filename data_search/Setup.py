e468b528171f9eb356d5b117da777412ec57589d
Christian Normann <christian50020@gmail.com>
2021-11-12 21:12:06 +0100

    removed data_search

--- data_search/Setup.py
+++
@@ -1,5 +0,0 @@
-class Setup:
-    def __init__(self):
-        self.DRIVER_PATH = "" #Chromedriver path e.g. .../Desktop/Scraping/chromedriver 2
-        self.amount_images = 100
-        self.Search_Term = "" #Term you want to search for
